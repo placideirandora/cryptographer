b849bac40c4ad61ea1b305c0afb745052d439c20
Placide IRANDORA <placideirandora@gmail.com>
2019-10-02 10:33:49 +0200

    Add the initial start point

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cryptographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Placide IRANDORA Is A Rwandan Software Engineer And Cybersecurity Specialist.");
+        }
+    }
+}
